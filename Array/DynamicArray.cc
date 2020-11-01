3d19d868cc5d9176b1ea665f41e71f1c059dd417
lpejakovic <luka.pejakovic@gmail.com>
2020-11-01 21:58:04 +0100

    Implementirane sve metode za upravljanje DynamicArray-em

--- Array/DynamicArray.cc
+++ Array/DynamicArray.cc
@@ -9,7 +9,7 @@ namespace practicecpp
         DynamicArray::DynamicArray(16);
     }
 
-    DynamicArray::DynamicArray(int initCapacity){
+    DynamicArray::DynamicArray(int initCapacity) : size(0){
         if (initCapacity < 1) {
             cout << "Reko sam NE MOŽE!!" << endl;
             exit(EXIT_FAILURE);
@@ -37,26 +37,88 @@ namespace practicecpp
     bool DynamicArray::IsEmpty(){
         return size == 0;
     }
+
     int DynamicArray::GetItemByIndex(int index){
-        return 0;
+        if(index < 0 || index >= size){
+            cout << "Index out of bounds. (" << index << ")" << endl;
+            exit(EXIT_FAILURE);
+        }
+        return array[index];
     }
+
     void DynamicArray::Push(int item){
-
+        Resize();
+        
+        array[size] = item;
+        size++;
     }
+
     void DynamicArray::Insert(int index, int item){
+        if(index < 0 || index >= size){
+            cout << "Index out of bounds. (" << index << ")" << endl;
+            exit(EXIT_FAILURE);
+        }
 
+        Resize();
+
+        for(int i = size; i > index; i--){
+            array[i] = array[i-1];
+        }
+
+        array[index] = item;
+        size++;
     }
+
     void DynamicArray::Prepend(int item){
+        Insert(0, item);
+    }
 
-    }
     int DynamicArray::Pop(){
-        return 0;
+        if(size == 0){
+            cout << "Nema više ničega. Try again." << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        size--;
+        int item = array[size];
+
+        Resize();
+
+        return item;
     }
+
     void DynamicArray::Delete(int index){
+        if(index < 0 || index >= size){
+            cout << "Index out of bounds. (" << index << ")" << endl;
+            exit(EXIT_FAILURE);
+        }
+        for(int i = index; i < size; i++){
+            array[i] = array[i+1];
+        }
+
+        size--;
+
+        Resize();
     }
+
     void DynamicArray::Remove(int item){
+        for(int i = 0; i < size; i++){
+            if(item == array[i]){
+                Delete(i);
+                i--;
+            }
+        }
     }
+
     int DynamicArray::Find(int item){
-        return 0;
+        int retVal = -1;
+        for(int i = 0; i < size; i++){
+            if(item == array[i]){
+                retVal = i;
+                break;
+            }
+        }
+
+        return retVal;
     }
 }
