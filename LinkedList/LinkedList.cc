eff86a999241c61af1d9587186fbd3bfb70ed7e1
lpejakovic <luka.pejakovic@gmail.com>
2020-11-24 20:15:50 +0100

    File for implementation of LinkedList and added Main.cpp

---
+++ LinkedList/LinkedList.cc
@@ -0,0 +1,9 @@
+#include "LinkedList.h"
+#include <iostream>
+
+using namespace std;
+
+namespace practicecpp 
+{
+
+}
---
+++ LinkedList/Main.cpp
@@ -0,0 +1,9 @@
+#include "LinkedList.h"
+#include "LinkedList.cc"
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Herro Worrd" << endl;
+	return 0;
+}
