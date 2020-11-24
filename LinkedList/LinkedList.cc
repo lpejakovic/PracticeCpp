92a49b0a51f24541949b410445ccf61efe9c2e32
lpejakovic <luka.pejakovic@gmail.com>
2020-11-24 20:23:13 +0100

    Implemented GetSize

--- LinkedList/LinkedList.cc
+++ LinkedList/LinkedList.cc
@@ -1,9 +1,19 @@
 #include "LinkedList.h"
-#include <iostream>
 
 using namespace std;
 
 namespace practicecpp 
 {
+    template<class T>
+    int LinkedList<T>::GetSize(){
+        int size = 0;
 
+        LinkedList<T> node = head;
+        while (node)
+        {
+            node = node.next();
+            size++;
+        }
+        return size;
+    }
 }
